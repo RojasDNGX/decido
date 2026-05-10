c5af9bea9b4e75f97ab8695c802d4c331a204e49
Seu nome/Projeto <thiago.rojas@dngx.com.br>
2026-05-09 23:38:47 -0300

    fix: allow google user content images in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['10.10.0.*', '192.168.*.*', '172.16.*.*'],
   serverExternalPackages: ['better-sqlite3'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
